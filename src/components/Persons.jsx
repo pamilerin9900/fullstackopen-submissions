bda702e6a0c148974ddd29500d89042483b0e0b2
pamilerin9900 <pamilerin9900@gmail.com>
2025-10-25 20:24:03 +0100

    Delete src directory

--- src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-// Persons.js
-const Persons = ({ persons }) => (
-  <ul>
-    {persons.map(person => (
-      <li key={person.name}>{person.name} — {person.number}</li>
-    ))}
-  </ul>
-)
-export default Persons
